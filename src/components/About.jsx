24e00d6b92dd98512b6c4db43f6b408d607e8d3c
kari-osk <karinaosuka@gmail.com>
2022-10-16 14:55:20 -0300

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <Link to='/'>Home</Link>
+      <h2>about page</h2>
+    </div>
+  )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import About from './components/About';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <HashRouter>
+    <Routes>
+      <Route path='/' element={<App />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes>
+  </HashRouter>
   </React.StrictMode>
 );
 
