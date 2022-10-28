4fd697f94b1b5348492ddc1d74b6cc7c4e2d0d9f
kari-osk <karinaosuka@gmail.com>
2022-10-28 15:52:35 -0300

    Alter tailwind config

--- src/data.jsx
+++ src/data.jsx
@@ -1,24 +1,30 @@
 export const data = [
   {
     id: 1,
-    title: 'Operador ternário',
+    title: 'Styled component',
     link: 'https://www.linkedin.com/feed/update/urn:li:activity:6944625366216896512/',
     image: 'https://media-exp1.licdn.com/dms/image/C4D22AQHYQzqao2WQyA/feedshare-shrink_800/0/1655727711105?e=1668643200&v=beta&t=cJ_-uUssVFLJMiCpDTvdOcIKe0tKCAdR4cYAkAfGKt8'
   },
   {
     id: 2,
+    title: 'Operador ternário',
+    link: 'https://www.linkedin.com/feed/update/urn:li:activity:6944625366216896512/',
+    image: 'https://media-exp1.licdn.com/dms/image/C4D22AQHYQzqao2WQyA/feedshare-shrink_800/0/1655727711105?e=1668643200&v=beta&t=cJ_-uUssVFLJMiCpDTvdOcIKe0tKCAdR4cYAkAfGKt8'
+  },
+  {
+    id: 3,
     title: 'Arrow fuction',
     link: 'https://www.linkedin.com/feed/update/urn:li:activity:6950800255453933569/',
     image: 'https://media-exp1.licdn.com/dms/image/C4D22AQGYxcCDtydPJQ/feedshare-shrink_800/0/1657199918611?e=1668643200&v=beta&t=7tj805kIOxqVjzolfXh9FNO7FBvom2VrMPRFcROKAgk'
   },
   {
-    id: 3,
+    id: 4,
     title: 'Componentes',
     link: 'https://www.linkedin.com/feed/update/urn:li:activity:6961322534172545024/',
     image: 'https://media-exp1.licdn.com/dms/image/C4D22AQHWdpPDaqwAxw/feedshare-shrink_800/0/1659708624574?e=1668643200&v=beta&t=mtdBexfY0iZXw2EUq7ME7S_2xU26ozvAlqhYo5kItwM'
   },
   {
-    id: 4,
+    id: 5,
     title: 'Fontes',
     link: 'https://www.linkedin.com/feed/update/urn:li:activity:6985245282825342976/',
     image: 'https://media-exp1.licdn.com/dms/image/C4D22AQFqtfGmniyrUw/feedshare-shrink_1280/0/1665412252929?e=1668643200&v=beta&t=YKXE819PEFWgDHmRfH_BC1YZIRQgVTrE-5vnnpyycgY'
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     fontFamily: {
+      'body': ['montserrat', 'sans-serif'],
       'sans': ['montserrat', 'sans-serif'],
       'serif': ['Tenor Sans', 'serif']
     },
@@ -10,6 +11,32 @@ module.exports = {
       'dark': '#121117',
       'light': '#E2E2E2',
       'orange': '#FF8B14',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    screens: {
+      'xs': { 'min': '375px', 'max': '639px' },
+      // => @media (min-width: 375px amd max-width:639px)
+      'sm': { 'min': '640px', 'max': '767px' },
+      // => @media (min-width: 640px and max-width: 767px)
+
+      'md': { 'min': '768px', 'max': '1023px' },
+      // => @media (min-width: 768px and max-width: 1023px) 
+
+      'lg': { 'min': '1024px', 'max': '1279px' },
+      // => @media (min-width: 1024px and max-width: 1279px)
+
+      'xl': { 'min': '1440px' },
+      // => @media (min 1440px ) 
+
     },
     extend: {},
   },
