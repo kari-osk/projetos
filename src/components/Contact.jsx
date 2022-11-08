a66e8663fee395ef7f0d8c735a4c3c10006843ed
kari-osk <karinaosuka@gmail.com>
2022-11-08 18:56:20 -0300

    Alter post components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       <footer className='container text-center md:text-left pt-[26px]'>
         <h2>Contato</h2>
         <span className='flex flex-row gap-4 justify-center'>
-          <a className='text-light text-2xl' href='www.linkedin.com/in/karinaosuka' target="_blank" rel="noreferrer" ><LinkedinFilled /></a>
+          <a className='text-light text-2xl' href='https://www.linkedin.com/in/karinaosuka' target="_blank" rel="noreferrer" ><LinkedinFilled /></a>
           <a className='text-light text-2xl' href='https://github.com/kari-osk' target="_blank" rel="noreferrer" >
             <GithubFilled />
           </a>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,9 +15,11 @@ const Post = () => {
     <div id='post' className='flex flex-col flex-wrap md:flex-row '>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
         <div className='w-full h-full border py-4 px-4'>
-          <h2>Posts</h2>
+          <h2>Postagens</h2>
           <p className=''>Aqui compartilho as postagens que criei para o Linkedin, sobre JavaScript, ReactJS e UX/UI. Acesse para ler todos os posts</p>
-          <button className='px-2 py-2 border hover:bg-dark hover:text-light'>Linkedin</button>
+          <button className='px-2 py-2 border hover:bg-dark hover:text-light'>
+            <a href='https://www.linkedin.com/in/karinaosuka' target="_blank" rel="noreferrer">Linkedin</a>
+          </button>
         </div>
         {post.map((post) => (
           <div className='' key={post.id}>
