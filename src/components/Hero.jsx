2b07f5141a88779056f14bfbe658ef0833598618
kari-osk <karinaosuka@gmail.com>
2022-12-29 16:08:50 -0300

    Alter hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <h2 className='text-5xl uppercase mr-4'>Hello</h2>
       </section>
       <section>
-        <p>Acredito que a tecnologia e o design caminham juntos, pois uma aplicação web, um software não são somente formados por códigos. Mas sim por um mundo interdisciplinar que envolveu escutar o usuário, observar, testar o produto, atender as necessidades e acessibilidades.
+        <p>Acredito que a tecnologia e o design caminham juntos, pois uma aplicação web, um software não são formados somente por códigos. Mas sim por um mundo interdisciplinar que envolveu escutar o usuário, observar, testar o produto, atender as necessidades e acessibilidades.
         </p>
         <p>
           Sou designer em transição de carreiras para desenvolvimento web, com conhecimento em JavaScript, React com Context Api e Redux, MySQL, MongoDb, Testing e metodologias ágeis Scrum.
